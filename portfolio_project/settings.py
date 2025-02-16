fb35c3ce827f0272c348d617960534e321a51478
Sulav Acharya <128365074+achsulav@users.noreply.github.com>
2025-02-16 09:55:10 +0545

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"  # For collected static files
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"

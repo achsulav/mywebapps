b10d3eac004f639480bbf52e47f9814453367507
Sulav Acharya <128365074+achsulav@users.noreply.github.com>
2025-02-16 10:24:15 +0545

    Update urls.py

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,7 +1,26 @@
+#from django.urls import path
+#from .views import home, error  # Import your views
+
+#urlpatterns = [
+  #  path('', home, name='home'),  # Home page
+ #   path('error/', error, name='error'),  # Error page  
+#]
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path
-from .views import home, error  # Import your views
+from portfolio_app import views
 
 urlpatterns = [
-    path('', home, name='home'),  # Home page
-    path('error/', error, name='error'),  # Error page  
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('', home, name='home'),
+    path('error/', error, name='error'),
+    
+    
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
